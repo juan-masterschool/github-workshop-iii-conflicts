acfa84a982f5f5a2267de2b6b5d529643b8c7392
Juan <167711474+juan-masterschool@users.noreply.github.com>
2026-06-28 14:36:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import statistics
-
-def average_calculation(elements):
-  average = statistics.mean(elements)
-  return average
-  
-
 def main():
-  goals_scored = [3, 2, 4, 0, 0, 1, 2]
-  average_goals = average_calculation(goals_scored)
-  print(f"The goals' average scored was {average_goals}!")
+  name = input("What's your name?: ")
 
 if __name__ == "__main__":
   main()
